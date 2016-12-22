5dd5aa4ab181ecc0e224a2b7b38455125deeae64
Jared Updike <jupdike@thebrain.com>
2016-12-22 12:25:01 -0500

    Indentation and split out into mycode.ts

--- src/client/scripts/album.tsx
+++ src/client/scripts/album.tsx
@@ -1,62 +1,12 @@
 /// <reference path="/Users/jupdike/Documents/dev/nitpic/node_modules/@types/jquery/index.d.ts" />
 /// <reference path="/Users/jupdike/Documents/dev/nitpic/node_modules/@types/react/index.d.ts" />
 
-var MyCode = (function() {
-
-function getField(desc, key, defaulty) {
-  var ret = defaulty;
-  // parse desc and grab g=c or g=n or g=e or g=w or g=s
-  var ps = desc.split(":");
-  if (ps && ps.length >= 1) {
-    ps.forEach(function(ab) {
-      var pair = ab.split("=");
-      if (pair && pair.length == 2) {
-        if (pair[0] === key) {
-          ret = pair[1];
-        }
-      }
-    })
-  }
-  return ret;
-}
-
-function makethumburl(fname, desc) {
-  desc = desc || "g=c";
-  var grav = getField(desc, "g", "c");
-  return "/thumbs" + "/sq" + grav + "." + fname;
-}
-
-function ajaxGetHelper(url, success) {
-  $.ajax({
-    url: url,
-    dataType: 'json',
-    cache: false,
-    success: success,
-    error: function(xhr, status, err) {
-      console.error(this.props.url, status, err.toString());
-    }.bind(this)
-  });
-}
-
-function ajaxPostHelper(url, data, success) {
-  $.ajax({
-    url: url,
-    dataType: 'json',
-    type: 'POST',
-    data: data,
-    success: success,
-    error: function(xhr, status, err) {
-      console.error(this.props.url, status, err.toString());
-    }.bind(this)
-  });
-}
-
 var Album = React.createClass({
   getInitialState: function() {
     return { data: { list: [], bykey: {} } };
   },
   componentDidMount: function() {
-    ajaxGetHelper(this.props.url,
+    MyCode.ajaxGetHelper(this.props.url,
       (data) => {
         this.setState({data: data});
       });
@@ -91,7 +41,7 @@ var Pic: any = React.createClass({
       newObj = { title: this.state.title, fname: this.state.fname, desc: arg };
     }
     // send this to the Model (on the server)
-    ajaxPostHelper('/api/pics', newObj,
+    MyCode.ajaxPostHelper('/api/pics', newObj,
       (data) => {
         //console.log("got back changed state from server");
         //console.log(data);
@@ -101,7 +51,7 @@ var Pic: any = React.createClass({
   render: function() {
     return (
       <div className="pic">
-        <img src={makethumburl(this.state.fname, this.state.desc)} />
+        <img src={MyCode.makethumburl(this.state.fname, this.state.desc)} />
         <Caption initValue={this.state.title} onDone={(arg) => this.handleCaptionDone('title', arg)} />
         <Caption initValue={this.state.desc} onDone={(arg) => this.handleCaptionDone('desc', arg)} />
       </div>
@@ -149,11 +99,3 @@ var Caption: any = React.createClass({
       );
   }
 });
-
-return {
-  Album: Album,
-  ajaxGetHelper: ajaxGetHelper,
-  makethumburl: makethumburl
-};
-
-})();
---
+++ src/client/scripts/mycode.ts
@@ -0,0 +1,50 @@
+class MyCode {
+
+  public static getField(desc, key, defaulty) {
+    var ret = defaulty;
+    // parse desc and grab g=c or g=n or g=e or g=w or g=s
+    var ps = desc.split(":");
+    if (ps && ps.length >= 1) {
+      ps.forEach(function(ab) {
+        var pair = ab.split("=");
+        if (pair && pair.length == 2) {
+          if (pair[0] === key) {
+            ret = pair[1];
+          }
+        }
+      });
+    }
+    return ret;
+  }
+
+  public static makethumburl(fname, desc) {
+    desc = desc || "g=c";
+    var grav = MyCode.getField(desc, "g", "c");
+    return "/thumbs" + "/sq" + grav + "." + fname;
+  }
+
+  public static ajaxGetHelper(url, success) {
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      cache: false,
+      success: success,
+      error: function(xhr, status, err) {
+      console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
+  public static ajaxPostHelper(url, data, success) {
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'POST',
+      data: data,
+      success: success,
+      error: function(xhr, status, err) {
+      console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+}
